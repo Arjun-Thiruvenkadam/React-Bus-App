dc59b76ed576cc836685876ff9d704be6a41490b
Arjun Thiruvenkadam <arjunthiru123@gmail.com>
2020-10-03 10:27:51 +0530

    E2E

--- cypress/integration/e2e_spec.js
+++ cypress/integration/e2e_spec.js
@@ -3,3 +3,26 @@ describe('The Home Page', () => {
     cy.visit('/');
   });
 });
+
+describe('Home Page to Orders page', () => {
+  it('successfully loads', () => {
+    cy.visit('/');
+    cy.get('.Navlink').click();
+  });
+});
+
+describe('Home Page to login page to order', () => {
+  it('successfully loads', () => {
+    cy.visit('/');
+    cy.get('a[href*="login"]').click();
+    cy.get('input[type="email"]').type('user1@gmail.com');
+    cy.get('input[type="password"]').type('password');
+    cy.get('form').within(() =>
+      cy
+        .get('button')
+        .should('have.class', 'Button Success')
+        .click(),
+    );
+    cy.get('a[href*="order"]').click();
+  });
+});
